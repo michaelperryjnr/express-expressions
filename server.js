f51a2d7e015dd3ce16665c563b10db970cbc081e
Michael Perry Tettey <michaelperryt97@gmail.com>
2023-12-17 21:38:29 +0000

    Minor update

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.post("/signup", async (req, res) => {
 
 mongoose
   .connect(
-    "mongodb+srv://thylme:12345@express-trial.jroaixj.mongodb.net/User?retryWrites=true&w=majority"
+    "mongodb+srv://thylme:12345@express-trial.jroaixj.mongodb.net/Node-API?retryWrites=true&w=majority"
   )
   .then(() => {
     console.log("Connected to database succesfully");
