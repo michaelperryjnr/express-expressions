3e961e0dfb1440182daa1adc7373fd14db157725
Michael Perry Tettey <michaelperryt97@gmail.com>
2023-12-17 22:08:39 +0000

    Added new request methods

--- server.js
+++ server.js
@@ -7,9 +7,21 @@ const app = express();
 
 app.use(express.json());
 
+//create user
 app.post("/signup", async (req, res) => {
   try {
     const user = await User.create(req.body);
+    res.status(200).json(user);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//getUSers in db
+app.get("/getUsers", async (req, res) => {
+  try {
+    const user = await User.find({});
     res.status(200).json(user);
   } catch (error) {
     console.log(error.message);
